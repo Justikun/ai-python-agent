81747b280ed4f2254af68fef36944c99b552bff5
Justin Lowry <8211769+Justikun@users.noreply.github.com>
2025-09-04 00:39:47 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai, types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = ""
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if len(sys.argv) == 1:
+        print("no arguments")
+        sys.exit(1)
+
+
+    print(response)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    main()
