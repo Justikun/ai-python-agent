d6b7b431f5bffbfd57657ea45529546c7a7455a2
Justin Lowry <8211769+Justikun@users.noreply.github.com>
2025-09-04 01:24:19 -0600

    add verbose

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai, types
+from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = ""
+    verbose = "--verbose" in sys.argv
+    args = []
+
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your promp" [--verbose]')
+        print('Example: python main.py "Be concise. Show me a for loop in Python."')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print("User prompt:", user_prompt)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+
+    generate_content(client, messages, verbose)
+
+
+
+def generate_content(client, messages, verbose):
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
 
-    if len(sys.argv) == 1:
-        print("no arguments")
-        sys.exit(1)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Reponse:")
+    print(response.text)
 
 
-    print(response)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
