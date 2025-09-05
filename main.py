d9d4a99d9b11df7db7ba4d0af08bfc8203ad6029
Justin Lowry <8211769+Justikun@users.noreply.github.com>
2025-09-04 20:14:47 -0600

    add system prompt

--- main.py
+++ main.py
@@ -38,16 +38,19 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    content_config = genai.types.GenerateContentConfig(system_instruction=system_prompt)
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=content_config,
     )
 
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Reponse:")
+    print("Response:")
     print(response.text)
 
 
